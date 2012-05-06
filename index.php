3d94ebada74fcc105c6cd5552c8c5e0dd76715c9
mari <mari@mari>
2012-05-06 16:54:13 +0400

    simle form

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
-<?php
-function arr_max($arr)
-{
-	$max = $arr[0];
-	for ($i=1; isset($arr[$i]); $i++)
-		if ($arr[$i]>$max)
-		$max = $arr[$i];
-	return $max;
-}
-$arr1 = array(1, 2, 3, 45, '6');
-$arr2 = 'hdghdgjh';
-echo arr_max($arr1)."<br>\n";
-echo $arr1[1]."<br>";
-echo arr_max($arr2);
+<?if (isset($_POST['search']))
+	$search = $_POST['search'];
+		else $search = NULL;
+	echo <<<_END
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Создание простой формы</title>
+<link href="/css/style.css" rel="stylesheet"/>
+</head>
+<body>
+	Вы искали : $search <br>
+	<form action="index.php" method="post">
+		<label>Search: <input type="text" name="search" /></label>
+		<input type="submit" value="Go!" class="buttonSubmit" />
+	</form>
+</body>
+</html>
+_END;
 ?>
