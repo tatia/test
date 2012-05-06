efa2d86410a9396e4bfc74524c38a50e3b5878f3
mari <mari@mari>
2012-05-06 23:36:55 +0400

    factorial

--- index.php
+++ index.php
@@ -1,21 +1,16 @@
-<?if (isset($_POST['search']))
-	$search = $_POST['search'];
-		else $search = NULL;
-	echo <<<_END
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Создание простой формы</title>
-<link href="/css/style.css" rel="stylesheet"/>
-</head>
-<body>
-	Вы искали : $search <br>
-	<form action="index.php" method="post">
-		<label>Search: <input type="text" name="search" /></label>
-		<input type="submit" value="Go!" class="buttonSubmit" />
-	</form>
-</body>
-</html>
-_END;
+
+<?
+function factorial($n)
+{
+	if ($n<0)	
+		{
+			return "Ошибка!Факториал числа $n не существет.";
+		}
+	else{
+		if($n == 0){return 1;}
+		return  $n*factorial($n-1);
+	}
+
+}
+echo factorial(0);
 ?>
